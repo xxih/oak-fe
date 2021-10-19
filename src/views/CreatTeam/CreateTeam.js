71f48ebcf7d6e98d92200114a09ee65b85465806
xxih <2217911664@qq.com>
2021-10-20 01:58:24 +0800

    feat: 完成创建队伍

--- src/views/CreatTeam/CreateTeam.js
+++ src/views/CreatTeam/CreateTeam.js
@@ -1,18 +1,62 @@
 import React from 'react'
-import { Layout  } from 'antd';
-// import style from './CreateTeam.module.scss'
-const { Header, Content, Footer } = Layout;
+import { Layout, Button, Input, Form } from 'antd';
+import style from './CreateTeam.module.scss'
+import {  CompressOutlined } from '@ant-design/icons';
+import { useHistory } from 'react-router';
+import api from '@/utils/api'
+
 
 export default function CreateTeam() {
-  return (
-    <Layout className="layout">
-        <Header>
-          <div className="logo" />
-        </Header>
-        <Content style={{ padding: '0 50px' }}>
-          <div className="site-layout-content">Content</div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-      </Layout>
+  const history = useHistory()
+  const [form] = Form.useForm()
+  function goBack(){
+    history.go(-1)
+  }  
+  function createTeam(){
+    let {teamName} = form.getFieldValue()
+    api.createTeam({
+      oakCode:localStorage.getItem('oakCode'),
+      teamName,
+      token:localStorage.getItem('token')
+    })
+    .then(()=>{
+      history.go(-1)
+    })
+  }
+  return (    
+  <div>
+    <Layout className={style.layout}> 
+      <div className={style.header}>
+        <div className={style.btnContainer}>
+          <CompressOutlined className={style.closeButton} onClick={goBack}></CompressOutlined>
+        </div>
+      </div>
+      <div className={style.content}>
+        <div className={style.container}>
+          <div className={style.title}>
+            新建团队详情
+          </div>
+          <div className={style.littleTitle}>
+            团队名称
+          </div>
+          <Form form={form}>
+            <Form.Item 
+              name="teamName"
+              rules={[{required:true}]}
+              
+            >
+              <Input style={{marginTop:"20px",width:340}} />
+            </Form.Item>
+          </Form>
+          <Button className={style.btn} 
+          type="primary"
+          onClick={createTeam}
+          >
+            创建团队
+          </Button>
+        </div>
+      </div>
+    </Layout>
+  </div>
   )
 }
