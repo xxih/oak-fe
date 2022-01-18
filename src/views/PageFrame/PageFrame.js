b6ddb0bbd7ed898bb770f590d55a0724a78e3f8a
xxih <2217911664@qq.com>
2022-01-18 17:01:19 +0800

    refactor: 抽离 header 组件

--- src/views/PageFrame/PageFrame.js
+++ src/views/PageFrame/PageFrame.js
@@ -1,136 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import {Route,Switch,useHistory,useLocation,Redirect} from 'react-router-dom'
-import { Menu,Layout,Dropdown,Avatar  } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
-import { useDispatch, useSelector } from 'react-redux';
-import { switchTeamAction } from '@/redux';
+import React from 'react'
+import {Route,Switch,useLocation,Redirect} from 'react-router-dom'
+import { Layout} from 'antd';
 
-
-import style from './PageFrame.module.scss'
+import MyHeader from './components/Header/MyHeader.js';
 import Project from './Project/Project.js'
 import Team from './Team/Team.js'
 import Mine from './Mine/Mine.js'
 import ProjectDetail from './ProjectDetail/ProjectDetail';
-import api from '@/utils/api'
-const { Header, Content,Footer  } = Layout;
+
+import style from './PageFrame.module.scss'
+
+const {  Content,Footer } = Layout;
 
 export default function PageFrame() {
 
-  const history = useHistory()
-  const location = useLocation()
-  const dispatch = useDispatch()
-  let selectedTeam = useSelector(state=>state.selectedTeam)
-  const [team, setTeam] = useState([])
-  useEffect(() => {
-    let res = api.getTeam({
-      token:sessionStorage.getItem('token'),
-      oakCode:sessionStorage.getItem('oakCode')
-    });
-    res.then((data)=>{
-      setTeam(data.team)
-    })
-  }, [])
-
-  function switchTeam(item){
-    dispatch(switchTeamAction(item.key))
-    history.push('/Project')
-  }
-  
-  function goCreateTeamPage(){
-    history.push('/CreateTeam')
-  }
-
-  function goMinePage(){
-    history.push('/Mine')
-  }
-
-  function goChangePassword(){
-    history.push('/ChangePassword')
-  }
-
-  const menu = (
-    <Menu>
-      <Menu.ItemGroup title={`切换团队`} style={{width:150}}>
-        <Menu.Divider />
-        {
-          team.map((item)=>{
-            return (<
-              Menu.Item key={item}
-              onClick={switchTeam}
-            >
-                <div>
-                  {item}
-                </div>
-              </Menu.Item>)
-          })
-        }
-      </Menu.ItemGroup>
-      <Menu.Divider />
-      <Menu.Item 
-        key="createNewTeam"
-        onClick={goCreateTeamPage}
-      >
-          新建团队
-      </Menu.Item>
-    </Menu>
-  );
-
-  const logOutMenu = (
-    <Menu>
-      <Menu.Item onClick={goMinePage} key="goMinePage">
-        个人中心
-      </Menu.Item>
-      <Menu.Item onClick={goChangePassword} key="goChangePassword">
-        修改密码
-      </Menu.Item>
-      <Menu.Item onClick={logOut} key="logout">
-        退出登录
-      </Menu.Item>
-    </Menu>
-  )
-
-  function logOut(){
-    sessionStorage.clear()
-    history.push('/Login')
-  }
-
-  function switchMenu(e){
-    history.push(e.key)
-  }
   return (
     <Layout className={style.layout}>
-      <Header className={style.header}>
-        <Dropdown overlay={menu} className={style.team}>
-          <a  href=" " onClick={e => e.preventDefault()}>
-            {selectedTeam}
-            <DownOutlined />
-          </a>
-        </Dropdown>
-        <Menu 
-        onClick={switchMenu} 
-        selectedKeys={location.pathname} 
-        mode="horizontal" 
-        className={style.menu}
-        >
-          <Menu.Item key="/Project">
-            项目
-          </Menu.Item>
-          <Menu.Item key="/Team" >
-            团队
-          </Menu.Item>
-          <Menu.Item key="/Mine">
-            我自己
-          </Menu.Item>
-        </Menu>
-        <Dropdown overlay={logOutMenu} className={style.avatar}>
-          <Avatar  size="large"
-          //  icon={<UserOutlined />} 
-            src={sessionStorage.getItem('avatar')}
-            style={{cursor:"pointer",marginTop:"10px"}}
-          />
-        </Dropdown>
-      </Header>
-
+      <MyHeader></MyHeader>
       <Content className={style.content}>
         <Switch>
           <Route path="/Project">
---
+++ src/views/PageFrame/components/Header/MyHeader.js
@@ -0,0 +1,143 @@
+import React, { useEffect, useState } from 'react'
+import {useHistory,useLocation} from 'react-router-dom'
+import { Menu,Dropdown,Avatar,Layout  } from 'antd';
+
+import { DownOutlined } from '@ant-design/icons';
+import { useDispatch, useSelector } from 'react-redux';
+import { switchTeamAction } from '@/redux';
+import api from '@/utils/api'
+
+import style from './MyHeader.module.scss'
+
+const {  Header } = Layout;
+
+export default function MyHeader() {
+  const location = useLocation()
+  const history = useHistory()
+  const dispatch = useDispatch()
+
+  //登录的时候已经 dispatch switchTeam，这里直接获取
+  let selectedTeam = useSelector(state=>state.selectedTeam)
+  const [team, setTeam] = useState([])
+  //登录后进入主页面就获取团队信息
+  useEffect(() => {
+    let res = api.getTeam({
+      token:sessionStorage.getItem('token'),
+      oakCode:sessionStorage.getItem('oakCode')
+    });
+    res.then((data)=>{
+      setTeam(data.team)
+    })
+  }, [])
+
+  const menu = (
+    <Menu>
+      <Menu.ItemGroup title={`切换团队`} style={{width:150}}>
+        <Menu.Divider />
+        {
+          team.map(item=>{
+            return (<Menu.Item 
+              key={item} 
+              onClick={switchTeam}
+            >
+              <div>
+                {item}
+              </div>
+            </Menu.Item>)
+          })
+        }
+      </Menu.ItemGroup>
+      <Menu.Divider />
+      <Menu.Item 
+        key="createNewTeam"
+        onClick={goCreateTeamPage}
+      >
+          新建团队
+      </Menu.Item>
+    </Menu>
+  );
+  
+  function switchTeam(item){
+    dispatch(switchTeamAction(item.key))
+    history.push('/Project')
+  }
+
+  function goCreateTeamPage(){
+    history.push('/CreateTeam')
+  }
+
+  /////////////////////////////////////////////////////////////////
+  const avatarMenu = (
+    <Menu>
+      <Menu.Item onClick={goMinePage} key="goMinePage">
+        个人中心
+      </Menu.Item>
+      <Menu.Item onClick={goChangePassword} key="goChangePassword">
+        修改密码
+      </Menu.Item>
+      <Menu.Item onClick={logOut} key="logout">
+        退出登录
+      </Menu.Item>
+    </Menu>
+  )
+
+  
+  function goMinePage(){
+    history.push('/Mine')
+  }
+
+  function goChangePassword(){
+    history.push('/ChangePassword')
+  }
+
+  
+  function logOut(){
+    sessionStorage.clear()
+    history.push('/Login')
+  }
+/////////////////////////////////////////////////////////////////
+
+
+
+  
+  function switchMenu(e){
+    history.push(e.key)
+  }
+
+
+  return (
+    <>
+      <Header className={style.header}>
+        <Dropdown overlay={menu} className={style.team}>
+          <a  href=" " onClick={e => e.preventDefault()}>
+            {selectedTeam}
+            <DownOutlined />
+          </a>
+        </Dropdown>
+        <Menu 
+          onClick={switchMenu} 
+          selectedKeys={location.pathname} 
+          mode="horizontal" 
+          className={style.menu}
+        >
+          <Menu.Item key="/Project">
+            项目
+          </Menu.Item>
+          <Menu.Item key="/Team" >
+            团队
+          </Menu.Item>
+          <Menu.Item key="/Mine">
+            我自己
+          </Menu.Item>
+        </Menu>
+        <Dropdown overlay={avatarMenu} className={style.avatar}>
+          <Avatar  size="large"
+          //  icon={<UserOutlined />} 
+            src={sessionStorage.getItem('avatar')}
+            style={{cursor:"pointer",marginTop:"10px"}}
+          />
+        </Dropdown>
+      </Header>
+    </>
+  )
+}
