a4b9c578dd239c12aeee40dfa98abb62a26e45b8
xxih <2217911664@qq.com>
2022-01-19 13:57:16 +0800

    refactor: 删去无用的 reducer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import RouterIndex from "./RouterIndex";
 
-import { useDispatch } from 'react-redux';
-// import { switchTeamAction, switchToken } from '@/redux';
-// import { useEffect } from "react";
 
 function App() {
-  
-  const dispatch = useDispatch()
-  // useEffect(() => {
-  //   dispatch(switchToken(false))
-  // }, [])
-
   return (
     <div className="App">
       <RouterIndex />
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,7 +1,5 @@
 
 const SWITCH_TEAM = 'SWITCH_TEAM'
-const SWITCH_TOKEN = 'SWITCH_TOKEN'
-
 //actions创建函数
 export function switchTeamAction(str){
   return{
@@ -9,25 +7,15 @@ export function switchTeamAction(str){
     teamName:str
   }
 }
-export function switchToken(token){
-  return{
-    type:SWITCH_TOKEN,
-    token
-  }
-}
-
 //初始值 是一个对象，可以包含多值
 const initialState = {
-  selectedTeam:'',
-  token:false
+  selectedTeam:''
 }
 
 export function reducer(state = initialState, action){
   switch(action.type){
     case SWITCH_TEAM:
       return {...state,selectedTeam:action.teamName}
-    case SWITCH_TOKEN:
-      return {...state,token:action.token}
     default:
       return state
   }
